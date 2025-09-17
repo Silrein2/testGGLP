fab54214066aee424f6910d6f82dd7d6c5638beb
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2025-09-17 08:27:44 +0800

    Import/export texts with key names

--- bee_safe/texts/admin.py
+++ bee_safe/texts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from modeltranslation.admin import TabbedTranslationAdmin
 from unfold.admin import ModelAdmin
+from unfold.contrib.import_export.forms import ExportForm
+from unfold.contrib.import_export.forms import ImportForm
 
 from bee_safe.custom_admin.admin import custom_admin
 
@@ -13,13 +15,16 @@ from .resources import TextResource
 @admin.register(Key)
 @admin.register(Key, site=custom_admin)
 class KeyAdmin(ModelAdmin, ImportExportModelAdmin):
-    pass
+    import_form_class = ImportForm
+    export_form_class = ExportForm
 
 
 @admin.register(Text)
 @admin.register(Text, site=custom_admin)
 class TextAdmin(ModelAdmin, ImportExportModelAdmin, TabbedTranslationAdmin):
     resource_class = TextResource
+    import_form_class = ImportForm
+    export_form_class = ExportForm
     list_display = ("key", "text", "order")
     ordering = ("key", "order")
     search_fields = ("key__name", "text")
--- bee_safe/texts/resources.py
+++ bee_safe/texts/resources.py
@@ -1,13 +1,38 @@
+from import_export import fields
 from import_export import resources
+from import_export.widgets import ForeignKeyWidget
+from modeltranslation.utils import get_translation_fields
 
 from bee_safe.contrib import mixins
 
+from .models import Key
 from .models import Text
+
+
+class KeyWidget(ForeignKeyWidget):
+    def clean(self, value, row=None, *args, **kwargs):
+        if not value:
+            return None
+        obj, _ = self.model.objects.get_or_create(**{self.field: value})
+        return obj
 
 
 class TextResource(
     mixins.TranslatedModelResourceMixin,
     resources.ModelResource,
 ):
+    key = fields.Field(
+        column_name="key",
+        attribute="key",
+        widget=KeyWidget(Key, field="name"),
+    )
+
     class Meta:
         model = Text
+        text_translation_fields = get_translation_fields("text")
+        fields = (
+            "id",
+            "key",
+            *tuple(text_translation_fields),
+            "order",
+        )

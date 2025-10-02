ff3169fea61acbb9b8f765c335595e12b0be1fe0
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2025-10-02 13:55:36 +0800

    Exempt CSRF on custom token create view

--- bee_safe/users/api/views.py
+++ bee_safe/users/api/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin
 from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.mixins import UpdateModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
@@ -75,6 +76,9 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
 
 
 class CustomTokenCreateView(DjoserTokenCreateView):
+    authentication_classes = [CsrfExemptSessionAuthentication]
+    permission_classes = [AllowAny]
+
     @extend_schema(
         request=CustomTokenRequestSerializer,
         responses={200: CustomTokenResponseSerializer},

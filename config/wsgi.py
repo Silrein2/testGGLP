296302fde4b314861bcf532abcaec43446685f9d
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2025-11-03 22:55:16 +0800

    Serve media files with whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,16 +2,11 @@
 WSGI config for bee_safe project.
 
 This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+and any production WSGI deployments. It exposes a module-level variable
+named ``application``. Django's ``runserver`` and WSGI servers discover
 this application via the ``WSGI_APPLICATION`` setting.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+This version uses WhiteNoise to serve static and media files for intranet-only deployment.
 """
 
 import os
@@ -19,14 +14,16 @@ import sys
 from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-# This allows easy placement of apps within the interior
-# bee_safe directory.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(BASE_DIR / "bee_safe"))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 application = get_wsgi_application()
+
+from config.settings import base
+
+application = WhiteNoise(application, root=base.STATIC_ROOT, prefix=base.STATIC_URL)
+application.add_files(base.MEDIA_ROOT, prefix=base.MEDIA_URL)
